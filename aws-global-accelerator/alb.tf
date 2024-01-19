77fa1890b2e7cd7d83139436e006134a2824af84
dsl2022 <2015rpro@gmail.com>
2024-01-18 23:42:35 -0500

    complete setup

--- aws-global-accelerator/alb.tf
+++ aws-global-accelerator/alb.tf
@@ -12,7 +12,7 @@ resource "aws_lb" "my_alb" {
 # ALB Target Group
 resource "aws_lb_target_group" "alb_tg" {
   name     = "my-alb-tg"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
@@ -38,5 +38,5 @@ resource "aws_lb_listener" "alb_listener" {
 resource "aws_lb_target_group_attachment" "tg_attachment" {
   target_group_arn = aws_lb_target_group.alb_tg.arn
   target_id        = aws_instance.primary_instance.id
-  port             = 80
+  port             = 8080
 }
--- aws-global-accelerator/sgs.tf
+++ aws-global-accelerator/sgs.tf
@@ -12,7 +12,20 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Adjust this to a more specific range for enhanced security
   }
-  
+   ingress {
+    description = "PORT 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust this to a more specific range for enhanced security
+  }
+    ingress {
+    description = "PORT 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust this to a more specific range for enhanced security
+  }
   # Default Egress Rule (Allow all outbound traffic)
   egress {
     from_port   = 0
